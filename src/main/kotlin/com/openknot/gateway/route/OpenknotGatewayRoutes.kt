12b8d08918876b43cf6bb21c28e510c5ab534ade
TaeHee00 <kancth03@gmail.com>
2025-11-16 19:30:24 +0900

    feat: gateway router 구현

---
+++ src/main/kotlin/com/openknot/gateway/route/OpenknotGatewayRoutes.kt
@@ -0,0 +1,40 @@
+package com.openknot.gateway.route
+
+import com.openknot.gateway.filter.AuthenticationFilter
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenknotGatewayRoutes(
+    private val authenticationFilter: AuthenticationFilter,
+) {
+    @Bean
+    fun gatewayRoutes(builder: RouteLocatorBuilder): RouteLocator =
+        builder.routes()
+            .route(
+                routeInfo = RouteInfo.USER_SERVICE,
+                requiresAuth = true
+            )
+            .route(
+                routeInfo = RouteInfo.AUTH_SERVICE,
+                requiresAuth = false
+            )
+            .build()
+
+    fun RouteLocatorBuilder.Builder.route(
+        routeInfo: RouteInfo,
+        requiresAuth: Boolean,
+    ): RouteLocatorBuilder.Builder =
+        this.route(routeInfo.name) { r ->
+            r.path(routeInfo.path)
+                .filters { f ->
+                    if (requiresAuth) {
+                        f.filter(authenticationFilter.filter())
+                    }
+                    f.stripPrefix(routeInfo.stripPrefix)
+                }
+                .uri(routeInfo.uri)
+        }
+}
